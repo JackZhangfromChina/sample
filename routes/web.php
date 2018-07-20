423ef8f43516aa7782a75eef31c98a94038995ca
vagrant <731547576@qq.com>
2018-07-20 03:58:44 +0000

    finishn layout and routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');
