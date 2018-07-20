5278612aec1636de7671953bbb6607eaffcf5838
vagrant <731547576@qq.com>
2018-07-20 10:33:33 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-  </head>
-  <body>
-    @include('layouts._header')
+</head>
+<body>
+@include('layouts._header')
 
-    <div class="container">
-      <div class="col-md-offset-1 col-md-10">
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
-      </div>
     </div>
-  </body>
+</div>
+</body>
 </html>
