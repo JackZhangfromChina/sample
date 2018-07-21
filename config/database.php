ee893229df9ad2368331b2f5ba47522208d9bbc6
vagrant <731547576@qq.com>
2018-07-21 01:21:01 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -30,7 +30,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
+    'default' => $db_config['connection'],
     'connections' => [
 
         'sqlite' => [
